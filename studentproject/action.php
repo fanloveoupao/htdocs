e8d42e8e2b6b38d0f50e94d74d65acb073fc0ba9
fanloveoupao <bruse_ios@163.com>
2017-04-09 23:33:50 +0800

    Initial commit

---
+++ studentproject/action.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bruse
+ * Date: 2017/4/9
+ * Time: 下午10:14
+ */
+
+require_once 'index.php';
+//通过action的值进行操作
+if (isset($_GET['action'])) {
+    switch ($_GET['action']) {
+        case "add":
+            Action::insert($_POST['name'], $_POST['sex'], $_POST['class_id'], $_POST['age']);
+            break;
+    }
+}
+
+class Action
+{
+    private static $sql_pdo = null;
+
+    public static function connect()
+    {
+        if (self::$sql_pdo == null) {
+            try {
+                self::$sql_pdo = new PDO("mysql:host=localhost;dbname=stu_db", 'root', '');
+                self::$sql_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$sql_pdo->query("set names utf8");
+                self::$sql_pdo->beginTransaction();
+            } catch (PDOException $exception) {
+                echo '<script >alert("数据库连接失败")</script>>';
+                die("连接失败" . $exception->getMessage());
+
+            }
+        }
+        return self::$sql_pdo;
+    }
+
+    public static function querry()
+    {
+        if (self::$sql_pdo == null) {
+            self::connect();
+        } else {
+            $sql = "select * from ys_stu";
+            $stm = self::$sql_pdo->prepare($sql);
+            $stm->execute();
+            return $stm->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+    }
+
+    public static function insert($name, $sex, $class_id, $age)
+    {
+        try {
+            $sql = "insert into ys_stu VALUE (?,?,?,?,?)";
+            $stmt = self::$sql_pdo->prepare($sql);
+            $arr = array(null, $class_id, $name, $sex, $age);
+            $stmt->execute($arr);
+            self::$sql_pdo->commit();
+
+        } catch (PDOException $exception) {
+            $str = $exception->getMessage();
+            echo '<script>alert("插入失败")</script>>';
+            die("插入失败.$str");
+            self::$sql_pdo->rollBack();
+        }
+    }
+}
+
---
+++ studentproject/index.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'action.php'
+?>
+<html>
+<head><title>学生管理系统</title></head>
+<body>
+<center>
+    <!--包含菜单-->
+    <?php
+    include('menu.php');
+    ?>
+    <h3>浏览学生信息</h3>
+    <!--表头-->
+    <table width="600" border="1">
+        <tr>
+            <th>ID</th>
+            <th>姓名</th>
+            <th>性别</th>
+            <th>班级</th>
+            <th>年龄</th>
+            <th>操作</th>
+        </tr>
+        <?
+        /*连接数据库*/
+        $sql_pdo = Action::connect();
+        /*查询数据库并且解析遍历显示*/
+        $arr = Action::querry();
+        foreach ($arr as $row) {
+            echo "<tr>";
+            echo "<td>{$row['id']}</td>";
+            echo "<td>{$row['name']}</td>";
+            echo "<td>{$row['sex']}</td>";
+            echo "<td>{$row['class_id']}</td>";
+            echo "<td>{$row['age']}</td>";
+            echo "<td>操作</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
+
+</center>
+</body>
+</html>
